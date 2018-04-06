4ae8a2ad00a1fcb4fb15aea51bf7c589df985fe0
Squishy123 <christian.gnaw@gmail.com>
2018-04-06 12:26:55 -0400

    Fixed remove and add counters

--- public/js/main.js
+++ public/js/main.js
@@ -5,13 +5,31 @@ let data = {
 };
 
 //add listener for button
-document.querySelector('button').addEventListener("click", () => {
+document.querySelector('#task').addEventListener("click", () => {
     let input = document.querySelector('input');
     if (input.value) {
         addTodo(input.value);
     }
     input.value = "";
 });
+
+/** 
+//add listener for button
+document.querySelector('.addTodo').addEventListener("click", () => {
+    let input = document.querySelector('input');
+    if (input.value) {
+        addTodo(input.value);
+    }
+    input.value = "";
+});
+
+//add listener for button
+document.querySelector('.removeTodo').addEventListener("click", () => {
+    let input = document.querySelector('input');
+    if (input.value) {
+        removeTodo(input.value);
+    }
+});*/
 
 function addTodo(value) {
     let i = data.todo.findIndex((e) => {
@@ -35,9 +53,13 @@ function removeTodo(value) {
     });
 
     if (i >= 0) {
-        console.log(i)
-        document.querySelector('#todo').removeChild(data.todo[i].node);
-        data.todo.splice(i, 1);
+        if (data.todo[i].num > 1) {
+            data.todo[i].num--;
+            data.todo[i].node.innerHTML = `${value}<span class="badge badge-primary badge-pill">${data.todo[i].num}</span>`
+        } else {
+            document.querySelector('#todo').removeChild(data.todo[i].node);
+            data.todo.splice(i, 1);
+        }
     }
 }
 
@@ -63,8 +85,12 @@ function removeCompleted(value) {
     });
 
     if (i >= 0) {
-        console.log(i)
-        document.querySelector('#completed').removeChild(data.completed[i].node);
-        data.completed.splice(i, 1);
+        if (data.completed[i].num > 1) {    
+            data.completed[i].num--;
+            data.completed[i].node.innerHTML = `${value}<span class="badge badge-primary badge-pill">${data.completed[i].num}</span>`
+        } else {
+            document.querySelector('#completed').removeChild(data.completed[i].node);
+            data.completed.splice(i, 1);
+        }
     }
 }
